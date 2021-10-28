3f1c5845f710ec2d8700d31a5bf39755a5759eb1
rosyidm123 <rosyidmuarif15@gmail.com>
2021-10-28 09:53:54 +0700

    Aplikasi Kotlin
    Tugas UTS

--- app/src/main/java/com/example/rosyidmuarif_20510009_tugasppb/MainActivity.kt
+++ app/src/main/java/com/example/rosyidmuarif_20510009_tugasppb/MainActivity.kt
@@ -5,17 +5,41 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import android.content.DialogInterface
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    fun submit_btn(View: View) {
+
+    fun submit_btn(view: View?) {
         val namaEditText = findViewById<EditText>(R.id.nama_edit_text)
         val nama = namaEditText.text.toString()
 
         val toast = Toast.makeText(applicationContext, nama, Toast.LENGTH_SHORT)
         toast.show()
+        AlertDialog.Builder(this)
+            .setIcon(R.mipmap.ic_launcher)
+            .setTitle("Aplikasi Kotlin")
+            .setMessage("Data Anda telah tersimpan")
+            .setPositiveButton("OK",
+                DialogInterface.OnClickListener { dialog, which ->
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Kamu memilih OK",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                })
+            .setNegativeButton("Cancel",
+                DialogInterface.OnClickListener { dialog, which ->
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Kamu memilih Cancel",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                })
+            .show()
     }
 }
